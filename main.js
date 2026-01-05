801f26d4eb0928f35ece3bd4e75f8ad4a7c6b1e4
Alex Ndaula <your_email@example.com>
2026-01-05 16:54:48 -0500

    Implement Simple Liker modal and event handling

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+document.addEventListener("DOMContentLoaded", () => {
+  // grab elements
+  const likeGlyph = document.getElementById("like-glyph");
+  const errorModal = document.getElementById("modal");
+  const errorMessage = document.getElementById("modal-message");
 
-// Your JavaScript code goes here!
+  // REQUIRED: hide error modal on load
+  errorModal.classList.add("hidden");
 
+  likeGlyph.addEventListener("click", () => {
+    // if heart is already activated, toggle off immediately
+    if (likeGlyph.classList.contains("activated-heart")) {
+      likeGlyph.classList.remove("activated-heart");
+      likeGlyph.textContent = "♡";
+      return;
+    }
 
+    // simulate server call
+    mimicServerCall()
+      .then(() => {
+        // SUCCESS
+        likeGlyph.classList.add("activated-heart");
+        likeGlyph.textContent = "♥";
+      })
+      .catch((error) => {
+        // FAILURE
+        errorMessage.textContent = error;
+        errorModal.classList.remove("hidden");
 
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
+        setTimeout(() => {
+          errorModal.classList.add("hidden");
+        }, 3000);
+      });
   });
-}
+});
